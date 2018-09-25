a81382039e6d906c3ebbc38037df9317770f2cdd
David Mills <dave@1morepx.com>
2018-09-25 09:41:31 -0400

    only keep numbers from arguments

--- adder.js
+++ adder.js
@@ -3,6 +3,6 @@
 // out the sum of them. If any argument is not a whole number,
 // skip it. Do support negative numbers though.
 
-var args = process.argv;
+var args = process.argv.slice(2);
 
 console.log("args", args);
