d7fa76960aa7cd72bc76b85fb1ed63698e8aa457
David Mills <dave@1morepx.com>
2018-09-25 09:33:08 -0400

    initial commit

---
+++ adder.js
@@ -0,0 +1,8 @@
+// Write a node program that takes in an unlimited number
+// of command line arguments, goes through each and prints
+// out the sum of them. If any argument is not a whole number,
+// skip it. Do support negative numbers though.
+
+var args = process.argv;
+
+console.log("args", args);
